b751d428450c5fd698696342c14812842a7fe529
mi3765 <chips1225@outlook.jp>
2023-07-09 14:42:08 +0900

    SidebarIcon.jsxを新規作成

---
+++ src/components/SidebarIcon.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Icon from "../images/画像1.svg";
+
+const SidebarIcon = () => {
+  return (
+    <div className="SidebarIcon">
+      <img src={Icon} alt="" />
+      <p>chips1225@outlook.jp</p>
+    </div>
+  );
+};
+
+export default SidebarIcon;
