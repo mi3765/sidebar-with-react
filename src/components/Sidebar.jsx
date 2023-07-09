9e37bfe3b384bb5cc247ff4c8a172219da303706
mi3765 <chips1225@outlook.jp>
2023-07-09 14:17:43 +0900

    Sidebar.jsxを編集

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { SidebarData } from "./SidebarData";
 
 function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <ul className="sidebarList"></ul>
+    </div>
+  );
 }
 
 export default Sidebar;
