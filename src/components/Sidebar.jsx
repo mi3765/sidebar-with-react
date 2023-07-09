277df834270fb32d08cf8e42bd21f34b73ba023f
mi3765 <chips1225@outlook.jp>
2023-07-09 14:43:35 +0900

    onClickでpathの切り替えを実装

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import { SidebarData } from "./SidebarData";
+import SidebarIcon from "./SidebarIcon";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <ul className="sidebarList"></ul>
+      <SidebarIcon></SidebarIcon>
+      <ul className="sidebarList">
+        {SidebarData.map((value, key) => {
+          return (
+            <li
+              key={key}
+              id={window.location.pathname === value.link ? "active" : ""}
+              className="row"
+              onClick={() => {
+                window.location.pathname = value.link;
+              }}
+            >
+              <div id="icon">{value.icon}</div>
+              <div id="title">{value.title}</div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
